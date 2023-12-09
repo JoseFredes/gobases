1427074986b7733bb9e569d3eda5b557694d27f9
JoseFredes <jose.fredes@mayor.cl>
2023-12-09 17:05:13 -0300

    feat:add vector operations and slices

--- vectors/vectors.go
+++ vectors/vectors.go
@@ -11,4 +11,18 @@ func Vectors(){
 
 	myarr[2] = 5
 	fmt.Println(myarr)
+
+
+	var silice1 []int
+	fmt.Println(silice1)
+	silice1 = append(silice1, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+	fmt.Println(silice1)
+	silice2 := silice1[1:3]
+	fmt.Println(silice2)
+	fmt.Println(&silice1[2])
+	fmt.Println(&silice2[1])
+	silice2[0] = 100
+	fmt.Println(silice1)
+	fmt.Println(silice2)
+
 }
